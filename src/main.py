4a74f1718df0f102c205df564335e791c11aa91f
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-11-03 23:27:26 -0300

    corrected Internal Server Error
    	* We've used flask's url_for(func) function in the html form
    	  in order to handle the request made by the user. It can
    	  change between a password randomly generated or a password
    	  generated by a dictionary file;
    	* We check if the user made a request and, if it really did,
    	  we then check the values associated with the radio buttons
    	  from hello.html to return the according type of password.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,13 @@
 from flask import Flask, render_template, request
 from password.generator import Generator
 
-
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('hello.html')
-
-
-@app.route('/show', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def gen_password():
-    # TODO redirect in case none option is chosen
-    if 'random' in request.form:
-        return Generator.gen_password_base64()
-    elif 'dict' in request.form:
-        return Generator.gen_password_by_dict(True)
+    if request.method == 'POST':
+        if request.form['password_gen_type'] == 'random':
+            return str(Generator.gen_password_base64(request.form.get('phrase')))
+        return str(Generator.gen_password_by_dict(request.form.get('trailing')))
+    return render_template('hello.html')
