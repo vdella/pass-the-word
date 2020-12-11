b6141e4ea6b1c210b1c0ac7187ee0341eb155a6f
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-12-11 18:28:38 -0300

    removed references to user_id.txt and label_id.txt

--- src/database.py
+++ src/database.py
@@ -5,12 +5,7 @@ cur = connection.cursor()
 
 
 def create_user(name: str, password: str):
-    with open('../user_id.txt', 'r') as f:
-        user_id: int = int(f.read())
-    with open('../user_id.txt', 'w') as f:
-        updated_user_id = user_id + 1
-        f.write(str(updated_user_id))
-    cur.execute('''INSERT INTO "user" (USER_ID, NAME, PASSWORD) VALUES (%s, %s, %s)''', (user_id, name, password))
+    cur.execute('''INSERT INTO "user" (NAME, PASSWORD) VALUES (%s, %s)''', (name, password))
     connection.commit()
 
 
@@ -23,14 +18,9 @@ def check_user(name: str):
     return None
 
 
-def create_label(name: str, content: str, user_id: int):
-    with open('../label_id.txt', 'r') as f:
-        label_id: int = int(f.read())
-    with open('../label_id.txt', 'w') as f:
-        updated_label_id = label_id + 1
-        f.write(str(updated_label_id))
-    cur.execute('''INSERT INTO "labels" (LABEL_ID, NAME, CONTENT, USER_ID) VALUES (%s, %s, %s, %s)''',
-                (label_id, name, content, user_id))
+def create_label(name: str, content: str, user_id: int) -> ():
+    cur.execute('''INSERT INTO "labels" (NAME, CONTENT, USER_ID) VALUES (%s, %s, %s)''',
+                (name, content, user_id))
     connection.commit()
 
 
